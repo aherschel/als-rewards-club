a4795b2d71ba574ac9f7083199159ce5247bf84c
Al Harris <ademp90@gmail.com>
2023-11-11 23:27:41 -0800

    fix: allow amplify components to render in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,29 @@ import type { Preview } from "@storybook/react";
 import '../styles/globals.css';
 import '@aws-amplify/ui-react/styles.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Amplify } from 'aws-amplify';
+import AmplifyConfig from '../amplifyconfiguration.json';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false;
+
+Amplify.configure({
+  API: {
+    GraphQL: {
+      endpoint: AmplifyConfig.aws_appsync_graphqlEndpoint,
+      region: AmplifyConfig.aws_appsync_region,
+      apiKey: AmplifyConfig.aws_appsync_apiKey,
+      defaultAuthMode: 'userPool',
+      modelIntrospection: AmplifyConfig.modelIntrospection as unknown as any,
+    },
+  },
+  Auth: {
+    Cognito: {
+      userPoolId: AmplifyConfig.aws_user_pools_id,
+      userPoolClientId: AmplifyConfig.aws_user_pools_web_client_id,
+    },
+  },
+});
 
 const preview: Preview = {
   parameters: {
